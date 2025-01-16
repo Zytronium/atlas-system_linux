000cbc692556cc018a9a1d56eff6e82e138d1688
Zytronium <dastelljes@outlook.com>
2025-01-15 19:04:26 -0600

    attempt to fix task 2

--- getline/_getline.c
+++ getline/_getline.c
@@ -11,7 +11,7 @@
 void *reset(char buffer[], ssize_t *bytes_in_buffer,
 		   ssize_t *buffer_index, char *line)
 {
-	memset(buffer, 0, sizeof(*buffer));
+	memset(buffer, 0, READ_SIZE);
 	*bytes_in_buffer = 0;
 	*buffer_index = 0;
 	free(line);
