65fc2fb820bd97eb170185a76f914837f9747c8e
Zytronium <dastelljes@outlook.com>
2025-01-15 19:05:44 -0600

    betty

--- getline/_getline.h
+++ getline/_getline.h
@@ -8,7 +8,8 @@
 
 #define READ_SIZE 2048
 
-void *reset(char buffer[], ssize_t *bytes_in_buffer, ssize_t *buffer_index, char *line);
+void *reset(char buffer[], ssize_t *bytes_in_buffer, ssize_t *buffer_index,
+			char *line);
 char *_getline(const int fd);
 
 
