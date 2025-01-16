f32b3b884da28c7c9bf3f8442ea5aaa29c204af2
Zytronium <dastelljes@outlook.com>
2025-01-15 18:35:54 -0600

    made the code terribly squished together and unreadable in order to satisfy betty formatting I hate betty formatting

--- getline/_getline.c
+++ getline/_getline.c
@@ -13,59 +13,34 @@ char *_getline(const int fd)
 	static ssize_t buffer_index;
 	char *line = NULL;
 	size_t line_size = 0;
-
 	if (fd < 0)
-	{
 		return (NULL);
-	}
-
 	while (1)
 	{
 		if (buffer_index >= bytes_in_buffer)
-		{
-			/* refill the buffer and reset index */
+		{ /* refill the buffer and reset index */
 			bytes_in_buffer = read(fd, buffer, READ_SIZE);
 			buffer_index = 0;
-
-			/* on EOF or error, return what's been read so far */
-			if (bytes_in_buffer <= 0)
-			{
-				return (line);
-			}
-		}
-
-		/* add chars from the buffer to line until newline, EOF, or error */
+			if (bytes_in_buffer <= 0) /* on EOF or error... */
+				return (line); /* return what's been read so far */
+		} /* add chars from the buffer to line until newline, EOF, or error */
 		while (buffer_index < bytes_in_buffer)
 		{
 			char c = buffer[buffer_index++];
-			/* return line once a newline is found */
-			if (c == '\n')
+			if (c == '\n') /* return line once a newline is found */
 			{
-				/* if line is empty, it hasn't been allocated yet. */
-				/* but since the 1st char is \n, we shouldn't return null */
-				/* because that would indicate that it reached EOF. */
 				if (line == NULL)
-				{
-					/* set line as empty str to avoid indicating EOF */
+				{ /* set line as empty str to avoid indicating EOF */
 					line = malloc(1);
 					if (line == NULL)
-					{
-						/* indicate error if malloc fails */
-						return (NULL);
-					}
+						return (NULL); /* indicate error if malloc fails */
 					line[0] = '\0';
 				}
 				return (line);
-			}
-
-			/* reallocate line to fit one more char (plus null-terminator) */
+			} /* reallocate line to fit one more char (plus null-terminator) */
 			line = realloc(line, line_size + 2);
-			/* if realloc failed, return null to indicate error */
-			if (line == NULL)
-			{
-				return (NULL);
-			}
-
+			if (line == NULL) /* if realloc failed.. */
+				return (NULL); /* return null to indicate error */
 			/* add this character to the line and null-terminate it */
 			line[line_size++] = c;
 			line[line_size] = '\0';
